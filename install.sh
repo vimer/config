09c4083f87fe8c7dee7278a6c74ec0664da3003e
kissjs <emacs@vimer.org>
2018-11-16 14:48:42 +0800

    fix

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ ln -s ./config/vim/ ~/.vim
 ln -s ./config/zsh/.zshrc ~/.zshrc
 ln -s ./config/vim/.vimrc ~/.vimrc
 ln -s ./config/tmux/.tmux.conf ~/.tmux.conf
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sht
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 rm -rf ~/.zshrc
 ln -s ./config/zsh/.zshrc ~/.zshrc
